1230b581144adfc4f71bf61a4445494a835a36fc
Evgeny Cherkashin <eugeneai@irnok.net>
2023-04-27 01:05:11 +0800

    Added MurMur2 hashing

--- lib/TrackerLib.hs
+++ lib/TrackerLib.hs
@@ -24,16 +24,44 @@ import Prelude.Compat
 import GHC.Generics (Generic)
 import qualified Control.Monad.Extra as Ex
 import Web.Scotty
-import Data.Aeson.Types
+import Data.Aeson.Types ( FromJSON(..), Value(String), ToJSON(..) )
+-- import Data.Aeson (FromJSON(..))
+import qualified Data.Digest.Murmur64 as MM
+import Data.Digest.Murmur64 (asWord64)
+import qualified Data.Text.Metrics as Met
+import qualified Control.Applicative as CA
 
 data Message = Answer
                {  id::U.UUID
                -- , lev::Float
-               , hash::Int
+               , mm::MM.Hash64
                } deriving (Show, Eq, Generic)
 
 instance FromJSON Message
+
+instance FromJSON MM.Hash64 where
+
+  parseJSON (String v) = case pp of
+                           Just uuid -> CA.pure . toHash $ uuid
+                           Nothing -> CA.empty
+    where
+      pp = U.fromText v
+  parseJSON _ = CA.empty
+
 instance ToJSON Message
+
+instance ToJSON MM.Hash64 where
+  toJSON :: MM.Hash64 -> Value
+  toJSON = String . U.toText . fromHash
+
+fromHash :: MM.Hash64 -> U.UUID
+fromHash = U.fromWords64 0 . MM.asWord64
+
+toHash :: U.UUID -> MM.Hash64
+toHash uuid = MM.hash64AddWord64 (snd rc) h0
+  where
+    rc = U.toWords64 uuid
+    h0 = MM.hash64 BL.empty
 
 msgServer :: IO ()
 msgServer = scotty 3333 $ do
@@ -52,7 +80,7 @@ msgServer = scotty 3333 $ do
     muuid <- liftIO $ UU.nextUUID
     case muuid of
       Just uuid -> do
-        let answer = Answer {id=uuid, hash=32}
+        let answer = Answer {id=uuid, mm=MM.hash64 b}
         -- html $ mconcat ["<pre>", (TLE.decodeUtf8 b), "</pre>", "/n", maybe "" (TL.fromStrict . U.toText) muuid]
         -- html $ mconcat ["<pre>", (TLE.decodeUtf8 b), "</pre>", "/n", maybe "" (TL.fromStrict . U.toText) muuid]
         json answer
